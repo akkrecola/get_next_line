55b902836ef6e3b3888305d94b064b15ae0c5964
Erkka Lehtoranta <elehtora@hive.fi>
2022-02-23 16:22:23 +0200

    Created program files (.c, .h, Makefile)

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 16:18:10 by elehtora          #+#    #+#             */
+/*   Updated: 2022/02/23 16:19:10 by elehtora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+/* Function prototypes */
+int		get_next_line(const int fd, char **line);
+
+#endif

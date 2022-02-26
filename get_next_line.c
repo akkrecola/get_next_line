459026f4dfa4dec3b0a1120ba232e7560525e226
Erkka Lehtoranta <elehtora@hive.fi>
2022-02-26 23:20:18 +0200

    Comments in C file on the purpose of the function.

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,20 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 03:47:18 by elehtora          #+#    #+#             */
-/*   Updated: 2022/02/25 03:47:19 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/02/26 23:20:11 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+/*
+ * get_next_line() reads line-by-line the file at file descriptor
+ * fd into a buffer.
+ */
+
+int	get_next_line(const int fd, char **line)
+{
+	static char	*line_stop;
+
+	
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,13 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 03:47:26 by elehtora          #+#    #+#             */
-/*   Updated: 2022/02/25 03:47:27 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/02/26 23:05:08 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+int	get_next_line(const int fd, char **line);
+
+#endif
